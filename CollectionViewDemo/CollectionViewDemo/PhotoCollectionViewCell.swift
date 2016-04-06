0184d560fb592a809c80b13a45333f8f13302618
kioko <kiokotomas@gmail.com>
2016-04-06 17:30:33 +0300

    Create collection view cell class and add outlet

---
+++ CollectionViewDemo/CollectionViewDemo/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CollectionViewDemo
+//
+//  Created by Kioko on 05/04/2016.
+//  Copyright © 2016 Thomas Kioko. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+}
